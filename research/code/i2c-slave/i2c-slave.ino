fe94a94005ab74f21b38bfec346e2a3f7e536a4e
Leonel <bohnetleonle@gmail.com>
2018-09-30 15:17:05 +0200

    bug fix

--- research/code/i2c-slave/i2c-slave.ino
+++ research/code/i2c-slave/i2c-slave.ino
@@ -3,13 +3,13 @@
 #include <Wire.h>
 
 int i2cAddress = 8;
-int example = 1234;
+int example = 0;
 
 void setup() {
   Wire.begin(i2cAddress);       // join i2c bus
   Wire.onReceive(recByteEvent); // register event
-//Wire.onRequest(sendByteEvent);// register event
-  pinMode(13, OUTPUT);          // defines onboard led as output
+  Wire.onRequest(sendByteEvent);// register event
+  pinMode(3, OUTPUT);          // defines onboard led as output
   Serial.begin(9600);
   Serial.write("It works");
 }
@@ -22,13 +22,13 @@ void loop() {
 // this function is registered as an event, see setup()
 void recByteEvent(int howMany) {
   while (1 < Wire.available()) {   // loop through all but the last
-    long recByte = Wire.read();    // receive byte as a character
+    char recByte = Wire.read();    // receive byte as a character
     Serial.print(recByte);         // print the received byte
-    if (recByte == 0000111)       // if the byte is 0000111, turn onboard led on
+    if (recByte == 585)        // if the byte is 0000111, turn onboard led on
     {
-      digitalWrite(13, HIGH);
+      digitalWrite(3, HIGH);
     }
-    else if (recByte == 0000100)  // if the byte is 0000100, turn onboard led off
+    else if (recByte == 00001000)  // if the byte is 0000100, turn onboard led off
     {
       digitalWrite(13, LOW);
     }
