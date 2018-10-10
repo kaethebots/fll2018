570e233d31c6360240cd6cfe8838cd31142c9b9d
Karl Beecken <karl.beecken@icloud.com>
2018-10-10 19:45:23 +0200

    add variable vz uuid

--- research/code/i2c-master/send-to-vz.sh
+++ research/code/i2c-master/send-to-vz.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+#
+# by karlbeecken
+#
+# usage: ./send-to-vz.sh > /dev/null &
+# debugging: just ./send-to-vz.sh
+#
+
 temp="22.75834845"
+vzuuid="af1e5b80-c70b-11e8-93fa-ddbac8086c38"
 
 while true
 do
 temp=`python test.py`
-wget -O - -q "http://localhost/middleware.php/data/af1e5b80-c70b-11e8-93fa-ddbac8086c38.json?operation=add&value=$temp" > /dev/null
+wget -O - -q "http://localhost/middleware.php/data/$vzuuid.json?operation=add&value=$temp" > /dev/null
 echo "$temp"
 sleep 1
 done
