507864c35811332873418a64a8579a1c69c5ea44
Karl Beecken <karl.beecken@icloud.com>
2018-10-13 14:04:49 +0200

    bug fix

--- research/code/i2c-master/send-to-vz.sh
+++ research/code/i2c-master/send-to-vz.sh
@@ -12,16 +12,16 @@ IFS=' '
 #hum="56.46357645"
 tempuuid="af1e5b80-c70b-11e8-93fa-ddbac8086c38"
 humuuid="ec0736b0-ce3a-11e8-983d-81145097ecf6"
-datain=`python read-float.py`
 #echo "$datain"
 
 while true
 do
-echo "$datain"
-read temp hum <<< $datain
-wget -O - -q "http://localhost/middleware.php/data/$tempuuid.json?operation=add&value=$temp" > /dev/null
-echo "$temp"
-wget -O - -q "http://localhost/middleware.php/data/$humuuid.json?operation=add&value=$hum" > /dev/null
-echo "$hum"
-sleep 0.5
+  datain=`python read-float.py`
+  echo "$datain"
+  read temp hum <<< $datain
+  wget -O - -q "http://localhost/middleware.php/data/$tempuuid.json?operation=add&value=$temp" > /dev/null
+  echo "$temp"
+  wget -O - -q "http://localhost/middleware.php/data/$humuuid.json?operation=add&value=$hum" > /dev/null
+  echo "$hum"
+  sleep 0.5
 done
