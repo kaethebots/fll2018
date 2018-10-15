784301f98e3f46e2e350b694acc8ab3350630949
Karl Beecken <karl.beecken@icloud.com>
2018-10-15 20:17:56 +0200

    next try with shell_exec

--- research/code/web/switch.php
+++ research/code/web/switch.php
@@ -1,6 +1,6 @@
 <?php
 
-exec(" python ../i2c-master/switch.py ($_GET["i2cAddress"]) ($_GET["device"])");
+shell_exec(" python ../i2c-master/switch.py ($_GET["i2cAddress"]) ($_GET["device"])" );
 header("Location:/ctrl/");
 
  ?>
