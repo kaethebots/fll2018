7a36cd546745b34484b6359858bd4f42559855bf
Karl Beecken <karl.beecken@icloud.com>
2018-10-16 17:49:13 +0200

    add protection

--- research/code/web/switch.php
+++ research/code/web/switch.php
@@ -1,6 +1,11 @@
 <?php
 
-shell_exec(" python ../i2c-master/switch.py ($_GET["i2cAddress"]) ($_GET["device"]) ");
+$addr_in = $_GET['i2cAddress'];
+$dev_in = $_GET['device'];
+$addr = escapeshellcmd($addr_in);
+$dev = escapeshellcmd($dev_in);
+
+exec("python ../i2c-master/switch.py $addr $dev");
 header("Location:/ctrl/");
 
  ?>
