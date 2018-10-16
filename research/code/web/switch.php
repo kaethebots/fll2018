c4c16088cf4281b2210ed1b8ad608720d8845436
Karl Beecken <karl.beecken@icloud.com>
2018-10-16 17:52:33 +0200

    bug fix for relative paths

--- research/code/web/switch.php
+++ research/code/web/switch.php
@@ -4,8 +4,10 @@ $addr_in = $_GET['i2cAddress'];
 $dev_in = $_GET['device'];
 $addr = escapeshellcmd($addr_in);
 $dev = escapeshellcmd($dev_in);
+$com_script = '../i2c-master/i2c-master.py';
+$exec_com_script = realpath($com_script);
 
-exec("python ../i2c-master/switch.py $addr $dev");
+exec("python $exec_com_script $addr $dev");
 header("Location:/ctrl/");
 
  ?>
