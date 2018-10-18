b766a42ed7bc5b59bd5d15bac02d9ca70f786620
Karl Beecken <karl.beecken@icloud.com>
2018-10-18 20:11:24 +0200

    add licence

--- research/code/i2c-master/i2c-master.py
+++ research/code/i2c-master/i2c-master.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
 #
-# can be helpful: http://wiki.erazor-zone.de/wiki:linux:python:smbus:doc
+#    fll2018 - code for a smart glasshouse
+#    Copyright (C) 2018  Karl Beecken
 #
-# modified by karlbeecken
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# can be helpful: http://wiki.erazor-zone.de/wiki:linux:python:smbus:doc
 #
 # usage: i2c-master.py <i2cAddress> <value to send>
 
